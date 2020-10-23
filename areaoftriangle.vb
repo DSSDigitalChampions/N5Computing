0128263adcfdec5f8d3f4d0d49a85a3cb78af330
DSSDigitalChampions <73368699+DSSDigitalChampions@users.noreply.github.com>
2020-10-23 23:47:01 +0100

    Update areaoftriangle.vb

--- areaoftriangle.vb
+++ areaoftriangle.vb
@@ -5,18 +5,22 @@ Module Program
 
         'Simple area of a triangle example.
 
-        'Variable Declaration
+        'Variable Declaration.
         Dim length As Single
         Dim breadth As Single
         Dim area As Single
 
+        'User Inputs.
         Console.WriteLine("Please enter length")
         length = Console.ReadLine
 
         Console.WriteLine("Please enter breadth")
         breadth = Console.ReadLine
 
+        'Calculate Area.
         area = length * breadth
+        
+        'Display Results.
         Console.WriteLine(area)
     End Sub
 End Module
